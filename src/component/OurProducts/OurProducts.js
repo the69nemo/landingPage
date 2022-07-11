7321ef7f8969c194dea1b67cbdd4c4539ea87863
the69nemo <thestigmata@yandex.ru>
2022-07-11 22:40:06 +0300

    our_products:_some_fix

--- src/component/OurProducts/OurProducts.js
+++ src/component/OurProducts/OurProducts.js
@@ -1,7 +1,7 @@
 import "./OurProducts.css";
-import MobileAppIcon from '../../images/mobile_app.png';
-import AccountIcon from '../../images/account.png';
-import OnlinePayIcon from '../../images/online_pay.png';
+import MobileAppIcon from "../../images/mobile_app.png";
+import AccountIcon from "../../images/account.png";
+import OnlinePayIcon from "../../images/online_pay.png";
 
 function OurProducts() {
   return (
@@ -19,15 +19,21 @@ function OurProducts() {
         <div className="our-products__links-wrapper">
           <a className="our-products__mobile" href="#">
             <img src={MobileAppIcon} alt="mobile app" />
+            <p className="our-products__mobile-text">Мобильное приложение</p>
           </a>
           <a className="our-products__account" href="#">
             <img src={AccountIcon} alt="account page" />
+            <p className="our-products__account-text">Учетная система</p>
           </a>
           <a className="our-products__online-register" href="#">
             <img src={AccountIcon} alt="online-register page" />
+            <p className="our-products___online-register-text">
+              Онлайн-запись для сайта и социальных сетей
+            </p>
           </a>
           <a className="our-products__online-pay" href="#">
             <img src={OnlinePayIcon} alt="online-pay page" />
+            <p className="our-products__online-pay-text">Онлайн-оплата</p>
           </a>
         </div>
         <div className="our-products__circle" />
